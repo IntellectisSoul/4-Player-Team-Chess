1fc641d3ad73b78482d513fa946bcbce6dc7a885
Jim <onward.pm@gmail.com>
2021-07-26 18:40:59 +0800

    populate pieces

--- Populate Pieces.js
+++ Populate Pieces.js
@@ -9,16 +9,18 @@ document.getElementById('i12').innerHTML = "♞";
 document.getElementById('e12').innerHTML = "♝";
 document.getElementById('h12').innerHTML = "♝";
 
-document.getElementById('f12').innerHTML = "♛";
-document.getElementById('g12').innerHTML = "♚";
+document.getElementById('f12').innerHTML = "♚";
+document.getElementById('g12').innerHTML = "♛";
 
-setTimeout(2000);
 { let lettr = ['c', 'd', 'e', 'f', 'g', 'h','i', 'j' ];
-for (i=0; i < lettr.length; i++) {
-x = (lettr[i] +11);
-document.getElementById(x).innerHTML = "♟";
+lettr.map(l =>
+    { x=l+11;
+     document.getElementById(x).innerHTML = "♟";}
+    )
 }
-}
+
+
+
 
 //Player 1 White Side
 document.getElementById('c1').innerHTML = "♖";
@@ -33,13 +35,13 @@ document.getElementById('h1').innerHTML = "♗";
 document.getElementById('f1').innerHTML = "♕";
 document.getElementById('g1').innerHTML = "♔";
 
+{ let lettr = ['c', 'd', 'e', 'f', 'g', 'h','i', 'j' ];
+lettr.map(l =>
+    { x=l+2;
+     document.getElementById(x).innerHTML = "♙";}
+    )
+}
 
-{let  lettr = ['c', 'd', 'e', 'f', 'g', 'h','i', 'j' ];
-for (i=0; i < lettr.length; i++) {
-x = (lettr[i] +2);
-document.getElementById(x).innerHTML = "♙";
-}
-}
 
 //Player 3 Black Side
 document.getElementById('a10').innerHTML = "♜";
@@ -51,16 +53,16 @@ document.getElementById('a4').innerHTML = "♞";
 document.getElementById('a8').innerHTML = "♝";
 document.getElementById('a5').innerHTML = "♝";
 
-document.getElementById('a7').innerHTML = "♛";
-document.getElementById('a6').innerHTML = "♚";
+document.getElementById('a7').innerHTML = "♚";
+document.getElementById('a6').innerHTML = "♛";
 
 { let no = [10, 9, 8, 7, 6, 5,4,3];
-    for (i=0; i < no.length; i++) {
-    x = ('b'+no[i]);
-    console.log(x);
-    document.getElementById(x).innerHTML = "♟";
-    }
-    }
+no.map(no =>
+    { x='b'+ no;
+     document.getElementById(x).innerHTML = "♟";}
+    )
+}
+
 
 //Player 4 White Side
 document.getElementById('l10').innerHTML = "♖";
@@ -77,12 +79,14 @@ document.getElementById('l6').innerHTML = "♔";
 
 
 { let no = [10, 9, 8, 7, 6, 5,4,3];
-for (i=0; i < no.length; i++) {
-x = ('k'+no[i]);
-console.log(x);
-document.getElementById(x).innerHTML = "♙";
-}
-}
+    no.map(no =>
+        { x='k'+ no;
+         document.getElementById(x).innerHTML = "♙";}
+        )
+    }
+    
+
+
 // let x = document.getElementByID("c12");
 
 // x.apendChild(♜);
